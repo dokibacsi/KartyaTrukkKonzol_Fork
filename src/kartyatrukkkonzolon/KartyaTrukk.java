5155a7d84a0b98364de7b8f561400e2e5a4134d8
dokibacsi <kvalcsi02@gmail.com>
2023-09-18 11:27:13 +0200

    oop-szemlelet alapjan rendezes

---
+++ src/kartyatrukkkonzolon/KartyaTrukk.java
@@ -0,0 +1,32 @@
+package kartyatrukkkonzolon;
+
+import java.util.Scanner;
+
+public class KartyaTrukk {
+    
+    private final Scanner sc = new Scanner(System.in);
+    Pakli pkl;
+ 
+    public KartyaTrukk() {
+        pkl = new Pakli();
+        for (int i = 0; i < 3; i++) {
+            pkl.kirak();
+            int oszlop = melyik();
+            pkl.kever(oszlop);
+        }
+        pkl.ezVolt();
+        
+    }
+
+    private int melyik() {
+        boolean jo;
+        int oszlop;
+        do {
+            System.out.print("melyik oszlop (1-3): ");
+            oszlop = sc.nextInt();
+            jo = oszlop >= 1 && oszlop <= 3;
+        } while (!jo);
+        return oszlop;
+    }
+
+}
